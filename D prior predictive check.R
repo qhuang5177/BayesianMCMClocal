6190491a88f2bbd29dded2811acc65acfc64fc1a
qhuang5177 <qhuang2024@163.com>
2025-04-08 12:39:19 +0100

    Initial commit

--- D prior predictive check.R
+++ D prior predictive check.R
@@ -1,6 +1,8 @@
 # 加载必要的包
 library(MASS)
 library(mgcv)
+library(ggplot2)
+
 
 
 set.seed(42)
@@ -23,7 +25,7 @@ S <- solve(S)
 ### 2. 模型总体设置
 I <- 23         # Number of region
 T <- nTimes     # Number of year
-N <- 10000     # Number of individuals in ith region and jth year 
+N <- 10000   # Number of individuals in ith region and jth year 
 p <- 3          # Number of covariates
 
 ### simulate hyperparameter
@@ -36,17 +38,19 @@ w0 <- mvrnorm(1, mu = rep(0, nKnots), Sigma = (1/tau0)*S)
 beta <- mvrnorm(1, mu = rep(0, p), Sigma = diag(10, p))
 
 # sigma_alpha^2 ~ InverseGamma(0.1,0.1)
-sigma2_alpha <- 1/rgamma(1, shape = 1, scale = 1)
+
+sigma2_alpha <- 1/rgamma(1, shape = 10, scale = 1)
 sigma_alpha <- sqrt(sigma2_alpha)
 
 ### Simulate f_it = eta_it + alpha_it for every region
+
 # dimension :23*14
 f_mat <- matrix(NA, nrow = I, ncol = T)
 
 #do it vectorized and get T calculate next level 
 for(i in 1:I){
   # For every region i，模拟 tau_i ~ Gamma(2.1, beta_tau)
-  tau_i <- rgamma(1, shape = 2.1, rate = beta_tau)
+  tau_i <- rgamma(1, shape = 5, rate = beta_tau)
   # simulate  w_i ~ Normal(w0, (1/tau_i)*S)
   w_i <- mvrnorm(1, mu = w0, Sigma = (1/tau_i)*S)
   eta_i <- as.vector(Z %*% w_i)
@@ -88,39 +92,27 @@ sim_data$p <- expit(sim_data$lp)
 # Simulate y ~ Bernoulli(p)
 sim_data$y <- rbinom(n = nrow(sim_data), size = 1, prob = sim_data$p)
 
-
-
-ibrary(ggplot2)
-
-# 假设你已经计算好某个区域的 eta_i, alpha_i 和 f_it（eta_i + alpha_i）
-# 例如：
-# T 为年份数（这里为 14）
 T <- 14
-# eta_i 为长度为 T 的向量，例如：
-eta_i <- as.vector(Z %*% mvrnorm(1, mu = w0, Sigma = (1/rgamma(1, shape = 2.1, rate = beta_tau))*S))
-# alpha_i 为长度为 T 的随机扰动
-alpha_i <- rnorm(T, mean = 0, sd = sigma_alpha)
-# f_it 为 eta_i 和 alpha_i 之和
-f_it <- eta_i + alpha_i
-df_plot <- data.frame(
-  Year = 1:T,
-  SmoothEffect = eta_i,
-  FinalEffect = f_it
-)
 
-# 绘制折线图：蓝色实线表示 eta_i（平滑效应），红色虚线表示 f_it（最终截距）
-ggplot(df_plot, aes(x = Year)) +
-  geom_line(aes(y = SmoothEffect, color = "Smooth Effect (eta)"), linewidth = 1) +
-  geom_point(aes(y = SmoothEffect, color = "Smooth Effect (eta)"), size = 2)  +
-  labs(title = "Trend of Smooth Effect vs. Final Effect",
-       x = "Year",
-       y = "Effect Value")  +
-  theme_minimal()
+par(mar=c(5, 4, 4, 4) + 0.3)  # 增加右边边距
+plot(1:T, eta_i, type="l", col="blue", ylab="eta_i", xlab="Time (Year)",
+     main="eta_i (left axis) and alpha_i (right axis) changing the mean of tao")
+par(new=TRUE)
+plot(1:T, alpha_i, type="l", col="red", axes=FALSE, xlab="", ylab="")
+axis(side=4)
+mtext("alpha_i", side=4, line=3)
+legend("topleft", legend=c("eta_i", "alpha_i"), col=c("blue", "red"), lty=1)
+
+#f_it <- eta_i + alpha_i
+#plot(1:T, f_it, type="l", col="green", main="f_it = eta + alpha", ylab="f_it", xlab="Year",lwd=2)
+#lines(1:T,eta_i,co='blue',lwd=2,lty=2)
 
 
 
 
 
+hist(sim_data$p, breaks = 50, col = "lightblue", main = "Prior Predictive of p change the mean of tao", xlab = "p")
 
+table(sim_data$y) / nrow(sim_data)  # y = 0 / 1 的比例
 
 
